562edd0c62501a781fb2626db40d13073df0dbfb
aumkar22 <aumkarlele22@gmail.com>
2021-09-07 06:38:53 +0200

    Patching [WIP, will be continued later]

--- src/preprocessing/patching.py
+++ src/preprocessing/patching.py
@@ -15,13 +15,9 @@ class Patching:
         self.scans = scans
         self.patch_size = patch_size
 
-
-    def check_if_all_no_tumour(self, patch_mask: np.ndarray):
-
-        if np.all(patch_mask == 0):
-
-
-
+    # def check_if_all_no_tumour(self, patch_mask: np.ndarray):
+    #
+    #     if np.all(patch_mask == 0):
 
     def random_center_tumor(self):
 
