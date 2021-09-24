7f73b14ac089d62caecf3f250b24ce8fa8ae4535
aumkar22 <aumkarlele22@gmail.com>
2021-09-24 12:03:15 +0200

    Fixed cropping and resizing dicom slices

--- src/scripts/classification_scripts/rsna_load_new.py
+++ src/scripts/classification_scripts/rsna_load_new.py
@@ -27,38 +27,20 @@ class RsnaLoad:
         self.resize_shape = resize_shape
 
     @staticmethod
-    def crop_dicom(dicom_array: np.ndarray) -> np.ndarray:
+    def dicom_load(dicom_file: str) -> np.ndarray:
         """
-        https://www.pyimagesearch.com/2016/04/11/finding-extreme-points-in-contours-with-opencv/
-        :param dicom_array:
+
+        :param dicom_file:
         :return:
         """
 
-        # thresh = cv2.threshold(dicom_array, 45, 255, cv2.THRESH_BINARY)[1]
-        # thresh = cv2.erode(thresh, None, iterations=2)
-        # thresh = cv2.dilate(thresh, None, iterations=2)
-
-        cnts = cv2.findContours(
-            dicom_array.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
-        )
-        cnts = imutils.grab_contours(cnts)
-        c = max(cnts, key=cv2.contourArea)
-
-        extLeft = tuple(c[c[:, :, 0].argmin()][0])
-        extRight = tuple(c[c[:, :, 0].argmax()][0])
-        extTop = tuple(c[c[:, :, 1].argmin()][0])
-        extBot = tuple(c[c[:, :, 1].argmax()][0])
-
-        return dicom_array[extTop[1] : extBot[1], extLeft[0] : extRight[0]]
-
-    def dicom_load(self, dicom_file: str) -> np.ndarray:
-        dicom_data = dicom.read_file(dicom_file).pixel_array
-        # breakpoint()
-        # cropped_dicom = self.crop_dicom(dicom_data)
-        resized_dicom = cv2.resize(dicom_data, (self.resize_shape, self.resize_shape))
-        return np.expand_dims(resized_dicom, axis=-1)
+        return dicom.read_file(dicom_file).pixel_array
 
     def data_load(self):
+        """
+
+        :return:
+        """
         dicom_path = Path(self.data_path / f"{self.patient}" / "FLAIR")
         dicom_list = [str(i) for i in dicom_path.glob(r"**/*.dcm")]
         dicom_list.sort(key=lambda x: int(x.split("-")[-1][:-4]))
@@ -71,32 +53,45 @@ class RsnaLoad:
         for dicom_index, dicom_file in enumerate(dicom_list[left_end:right_end]):
             dicom_data.append(self.dicom_load(dicom_file))
 
-        # breakpoint()
         dicom_volume = np.stack(dicom_data)
         preprocessed_dicom_volume = self.perform_preprocess(dicom_volume)
+        cropped_dicom = self.crop_slice(
+            preprocessed_dicom_volume, preprocessed_dicom_volume > 0
+        )
 
-        trimmed = self.trim(preprocessed_dicom_volume, preprocessed_dicom_volume != 0)
-        trimmed_ = np.split(trimmed, preprocessed_dicom_volume.shape[-1], -1)
+        resize_cropped_dicom = np.expand_dims(
+            np.array(
+                [
+                    cv2.resize(dicom_slice, (self.resize_shape, self.resize_shape))
+                    for dicom_slice in cropped_dicom
+                ]
+            ),
+            axis=-1,
+        )
         breakpoint()
 
     @staticmethod
     def perform_preprocess(image_volume: np.ndarray):
+        """
+
+        :param image_volume:
+        :return:
+        """
         pre = ImagePreProcess(image_volume)
         preprocessed_image = pre.apply_preprocess()
         return preprocessed_image.reshape(image_volume.shape)
 
     @staticmethod
-    def trim(arr, mask):
-        bounding_box = tuple(
-            slice(np.min(indexes), np.max(indexes) + 1) for indexes in np.where(mask)
-        )
-        return arr[bounding_box]
-
-
-# if __name__ == "__main__":
-#
-#     import matplotlib.pyplot as plt
-#
-#     plt.imshow(np.squeeze(dicom_data[0]), cmap="gray")
-#     plt.imshow(np.squeeze(preprocessed_dicom_volume[0]), cmap="gray")
-#     plt.show()
+    def crop_slice(arr, mask):
+        """
+        
+        :param arr:
+        :param mask:
+        :return:
+        """
+        return arr[
+            tuple(
+                slice(np.min(indexes), np.max(indexes) + 1)
+                for indexes in np.where(mask)
+            )
+        ]
