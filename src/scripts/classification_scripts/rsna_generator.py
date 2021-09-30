ebb800916e1ac1b924d16286445dddfb4c73018f
aumkar22 <aumkarlele22@gmail.com>
2021-09-30 09:00:18 +0200

    batch generator update

--- src/scripts/classification_scripts/rsna_generator.py
+++ src/scripts/classification_scripts/rsna_generator.py
@@ -1,71 +1,63 @@
 import math
-import random
-import tensorflow as tf
-import pandas as pd
+import numpy as np
 
-from typing import NoReturn
-from pathlib import Path
+from typing import Union, Tuple, List
 from tensorflow.keras.utils import Sequence
 
 
 class RsnaDataGenerator(Sequence):
+    """
+    RSNA batch generator class
+    """
+
     def __init__(
-        self,
-        data: pd.DataFrame,
-        batch_size: int,
-        dicom_path: Path,
-        shuffle: bool = True,
-        train: bool = False,
+        self, data_path: List[str], batch_size: int, train: bool = False,
     ):
 
+        """
+        :param data_path: List of paths to numpy preprocessed data
+        :param batch_size: Batch size
+        :param train: True for train/validation, False for test
+        """
+
+        self.data_path = data_path
         self.batch_size = batch_size
-        self.data = data
-        self.dicom_path = dicom_path
-        self.patient_ids = self.data["BraTS21ID"].str.zfill(5).values
-        self.label = self.data["MGMT_value"].values
-        self.shuffle = shuffle
         self.train = train
-        self.modalities = ["FLAIR", "T1w", "T1wCE", "T2w"]
-        self._shuffle_indices(self.shuffle)
 
     def __len__(self) -> int:
         """
         Calculate the number of batches that can be created using the provided batch size.
+        
         :return: Number of batches.
         """
-        return int(math.ceil(len(self.data) / self.batch_size))
+        return int(math.ceil(len(self.data_path) / self.batch_size))
 
-    def __getitem__(self, index: int):
+    def __getitem__(
+        self, index: int
+    ) -> Union[Tuple[np.ndarray, np.ndarray], np.ndarray]:
+
+        """
+        Create and return a batch of data and corresponding labels.
+
+        :param index: Batch index
+        :return: A tuple consisting of a batch of feature data and labels. During testing,
+        only feature batch data is returned
+        """
 
         start_index = index * self.batch_size
         end_index = (index + 1) * self.batch_size
-        batch_indices = self.patient_ids[start_index:end_index]
+        batch_path = self.data_path[start_index:end_index]
 
-        # feature_batch =
-
-    def on_epoch_end(self) -> NoReturn:
-        """
-        Method that is called once training has finished an epoch. The only thing to do in
-        those situations is shuffling the indices if that's been enabled in the constructor.
-        :return: No return.
-        """
-        super().on_epoch_end()
-        self._shuffle_indices(self.shuffle)
-
-    def _shuffle_indices(self, shuffle) -> NoReturn:
-        """
-        Shuffle indices if shuffle is True. Shuffle has been added as an explicit parameter instead
-        of just using self.shuffle to provide more intuition from callsites that shuffling is
-        optional.
-        :param shuffle: Whether or not data should be shuffled.
-        :return: No return. Data is shuffled inplace.
-        """
-        if shuffle:
-            random.shuffle(self.indices)
-
-    def load_dicom(self, patient_indices: str):
-
-        dicom_dataset = tf.data.Dataset.list_files(
-            str(self.dicom_path) + f"/{patient_indices}/"
+        feature_batch = np.array(
+            [
+                np.expand_dims(np.load(feature)["flair_volume"], axis=-1)
+                for feature in batch_path
+            ]
         )
-        dicom_bytes = tf.io.read_file()
+        if self.train:
+            label_batch = np.array(
+                [np.load(feature)["label"] for feature in batch_path]
+            )
+            return feature_batch, label_batch
+        else:
+            return feature_batch
